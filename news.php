94f4d09342795d808f2d97a8e6e7b55e4302f680
Kels <kelson.kien@me.com>
2013-02-21 10:25:31 +0100

    Create news.php

---
+++ news.php
@@ -0,0 +1,19 @@
+<div id="news" class="span12">
+  				<p>
+						Hellow les accros !
+					</p>
+
+					<p>
+						Anime Mobile ? WTF is that s*** ?! <br>
+						Et oui, encore un site dédié aux animes ! Attendez !!! Ne partez pas de suite ;)<br> 
+					</p>
+
+					<p>
+						Anime Mobile est destiné et optimisé pour les Smartphones ^^<br>
+						Bon Okay, t'es sur PC... Mais rassures toi, tu pourras jouir, toi aussi, des fonctionnalités de ce site =D
+					</p>
+
+					<p>
+						Sur ce, place aux animes ! 
+					</p>	
+</div>
