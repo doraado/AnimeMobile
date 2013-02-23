81e5fef8de6a45165587a8fe19b9968ae2a0dca9
Kelson Kien <kelson.kien@me.com>
2013-02-23 02:32:33 +0100

    fix up

--- upload.php
+++ upload.php
@@ -1,20 +1,20 @@
 <?php 
 	set_time_limit(0);
 	require_once 'Zend/Loader.php'; 
-	Zend_Loader::loadClass('Zend_Gdata_Youtube');
+	Zend_Loader::loadClass('Zend_Gdata_YouTube');
 	Zend_Loader::loadClass('Zend_Gdata_AuthSub');
 
 	$key = 'AIzaSyCxgPUZRCAc854zRs__tg6jYgqkm3OUADQ';
 
 	session_start();
 
-	if(isset($_SESSION['youtubetoken'])){
-		$token = $_SESSION['youtubetoken'];
+	if(isset($_SESSION['YouTubetoken'])){
+		$token = $_SESSION['YouTubetoken'];
 	}
 	else{
 		if(!isset($_GET['token'])){
 			$next = "http://109.232.235.200/AnimeMobile/upload.php";
-			$scope = "http://gdata.youtube.com";
+			$scope = "http://gdata.YouTube.com";
 			$secure = false;
 			$session = true;
 
@@ -23,19 +23,19 @@
 		else{
 			$token = $_GET['token'];
 			//$token = Zend_Gdata_AuthSub::getAuthSubSessionToken($_GET['token']);
-			$_SESSION['youtubetoken'] = $token;
+			$_SESSION['YouTubetoken'] = $token;
 		}	
 	}
 
 	$httpClient = Zend_Gdata_AuthSub::getHttpClient($token);
 	
-	$yt = new Zend_Gdata_Youtube($httpClient, 'Anime On Mobile', null, $key);
+	$yt = new Zend_Gdata_YouTube($httpClient, 'Anime On Mobile', null, $key);
 
 	$source = $yt->newMediaFileSource('video/009-1_01_vostfr_.mp4');
 	$source->setContentType('video/mp4');
 	$source->setSlug('009-1_01_vostfr_.mp4');
 
-	$video = new Zend_Gdata_Youtube_VideoEntry();
+	$video = new Zend_Gdata_YouTube_VideoEntry();
 	$video->setMediaSource($source);
 	$video->setVideoTitle('009-1_01_vostfr_');
 	$video->setVideoDescription('009-1_01_vostfr_ upload by Anime on Mobile');
@@ -43,10 +43,10 @@
 	$video->setVideoTags('anime, manga, 009-1, 01, vostfr');
 	$video->setVideoPrivate();
 
-	$url_upload = 'http://uploads.gdata.youtube.com/feeds/api/users/default/uploads';
+	$url_upload = 'http://uploads.gdata.YouTube.com/feeds/api/users/default/uploads';
 
 	try{
-		$newEntry = $yt->insertEntry($video, $url_upload, 'Zend_Gdata_Youtube_VideoEntry');
+		$newEntry = $yt->insertEntry($video, $url_upload, 'Zend_Gdata_YouTube_VideoEntry');
 
 		die($newEntry->getVideoId());
 	}catch(Zend_Gdata_App_HttpException $e){
